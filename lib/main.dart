042b3a7d72dc8d5e73342777cd1aaaf155c14b14
Gangashankar <bigdatamonk@gmail.com>
2020-06-20 14:04:52 +0530

    Magic 8 Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Answer(),
       ),
     );
+
+class Answer extends StatefulWidget {
+  @override
+  _AnswerState createState() => _AnswerState();
+}
+
+class _AnswerState extends State<Answer> {
+  int randomImageNum = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Ask me anything"),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      randomImageNum = Random().nextInt(5) + 1;
+                    });
+
+                    //print("I am magic ball");
+                  },
+                  child: Image.asset('images/ball$randomImageNum.png'),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
